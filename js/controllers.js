f7bda56f3b72c8a24a91e928d200f7980db1e8ac
jagruti181 <patiljagruti181@gmail.com>
2014-10-13 15:55:05 +0530

    First Live
    First Live

---
+++ js/controllers.js
@@ -0,0 +1,548 @@
+angular.module('starter.controllers', ['myservices'])
+
+.controller('HomeCtrl', function ($scope, $stateParams, MyServices) {
+
+    var slidersuccess = function (data, status) {
+        $scope.sliders = data;
+    };
+    MyServices.getallslider().success(slidersuccess);
+
+})
+
+.controller('DashCtrl', function ($scope, $stateParams, MyServices) {
+
+    var authenticate = function (data, status) {
+        console.log(data);
+        if (data != "false") {
+            $scope.loginlogouttext = "Logout";
+        }
+    };
+    MyServices.authenticate().success(authenticate);
+
+})
+
+.controller('ItemCtrl', function ($scope, $stateParams, MyServices) {
+
+    var authenticate = function (data, status) {
+        console.log(data);
+        if (data != "false") {
+            $scope.loginlogouttext = "Logout";
+        }
+    };
+    MyServices.authenticate().success(authenticate);
+    var categoryId = $stateParams.cid;
+
+    $scope.productItem = [];
+    var change = 11;
+    var counter = 0;
+    $scope.products = [];
+    var onsuccess = function (data, status) {
+        $scope.products = data;
+        $scope.loadMore();
+
+    };
+    MyServices.getproductbycategory(categoryId).success(onsuccess);
+
+    $scope.loadMore = function () {
+        var sum = counter + change;
+        if (sum > $scope.products.product.length) {
+            sum = $scope.products.product.length;
+        }
+        for (var i = counter; i <= sum; i++) {
+            $scope.productItem.push($scope.products.product[i]);
+        };
+        counter += change + 1;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    };
+
+
+})
+
+.controller('ProductCtrl', function ($scope, $stateParams, $ionicSlideBoxDelegate, $ionicPopup, $timeout, $ionicLoading, MyServices) {
+
+    $scope.showPopup = function () {
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<div class="text-center"><h1 class="ion-ios7-checkmark balanced"></h1><p>' + $scope.item.product.name + ' added to cart!</p>',
+            title: 'Added to cart!',
+            scope: $scope,
+
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 1500);
+    };
+
+    //Loader
+    $ionicLoading.show({
+        template: 'Loading...',
+        animation: 'fade-in',
+        showBackdrop: false,
+        maxWidth: 200,
+        showDelay: 500
+    });
+
+    var productId = $stateParams.pid;
+
+    var onsuccess = function (data, status) {
+        $scope.item = data;
+        $scope.item.product.quantity2 = 1;
+        console.log(data);
+    };
+    MyServices.getproductdetails(productId).success(onsuccess);
+
+    //Add to cart
+    $scope.addtocart = function (id, name, price, quantity) {
+
+        MyServices.addtocart(id, name, price, quantity);
+        $scope.showPopup();
+
+    };
+
+    //SLIDE BOX
+    $scope.nextSlide = function () {
+        $ionicSlideBoxDelegate.next();
+    };
+    $scope.prevSlide = function () {
+        $ionicSlideBoxDelegate.previous();
+    };
+    $timeout(function () {
+        $ionicSlideBoxDelegate.update();
+        $ionicLoading.hide();
+    }, 2000);
+})
+
+.controller('LookbookCtrl', function ($scope, $stateParams) {})
+
+.controller('LookbookitemCtrl', function ($scope) {})
+
+.controller('AccountCtrl', function ($scope) {
+
+})
+
+.controller('CartCtrl', function ($scope, $stateParams, MyServices) {
+    //Product details
+    $scope.subtotal=0;
+    var onproductsuccess = function (data, status) {
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].id == data.product.id) {
+                $scope.products[i].image = data.productimage[0].image;
+            }
+        }
+    };
+
+    
+     var getsubtotal = function (data, status) {
+        console.log(data);
+        $scope.subtotal = parseFloat(data);
+        calcdiscountamount();
+
+    };
+    MyServices.totalcart().success(getsubtotal);
+    var onsuccess = function (data, status) {
+        $scope.products = data;
+
+        for (var i = 0; i < data.length; i++) {
+            MyServices.getproductdetails(data[i].id).success(onproductsuccess);
+        }
+
+    };
+    MyServices.getcart().success(onsuccess);
+    //coupon
+
+    $scope.discountamount = 0;
+
+    function calcdiscountamount() {
+        var data = MyServices.getcoupondetails();
+        var subtotal = parseFloat($scope.subtotal);
+        console.log($scope.subtotal);
+        console.log(data);
+        if (data.coupontype == '1') {
+            if (data.coupontype.discountpercent != '0') {
+                $scope.ispercent = parseFloat(data.discountpercent);
+                $scope.discountamount = (subtotal * $scope.ispercent / 100);
+            } else {
+                $scope.isamount = parseFloat(data.discountamount);
+                $scope.discountamount = $scope.isamount;
+            }
+        }
+        if (data.coupontype == 4) {
+            $scope.isfreedelivery = "Free Delivery";
+            $scope.discountamount = 0;
+        }
+    };
+
+
+    var couponsuccess = function (data, status) {
+        console.log(data);
+        if (data == 'false') {
+            //$scope.validcouponcode = 0;
+        } else {
+            console.log("Show it");
+            //$scope.validcouponcode = 1;
+
+            MyServices.setcoupondetails(data);
+            calcdiscountamount();
+
+        }
+    }
+
+
+
+    $scope.checkcoupon = function (couponcode) {
+        MyServices.getdiscountcoupon(couponcode).success(couponsuccess);
+    };
+
+    //Remove item
+    var ondeletesuccess = function () {
+        MyServices.getcart().success(onsuccess);
+    };
+    $scope.deletecart = function (id) {
+        MyServices.deletecartfromsession(id).success(ondeletesuccess);
+    };
+
+    //Total
+    var ontotalsuccess = function (data, status) {
+        $scope.gettotal = data;
+    };
+    MyServices.totalcart().success(ontotalsuccess);
+
+})
+
+.controller('ContactCtrl', function ($scope, $ionicPopup, $timeout, MyServices) {
+
+    $scope.contact = {
+        name: "",
+        phone: "",
+        email: "",
+        comment: ""
+    };
+    $scope.showPopup = function () {
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<div class="text-center"><h1 class="ion-ios7-checkmark balanced"></h1><p>Thank you for your comment.</p>',
+            title: 'Message Sent!',
+            scope: $scope,
+
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 1.5 seconds for some reason
+        }, 1500);
+    };
+
+    var contactfun = function (data, status) {
+        console.log(data);
+        $scope.contact = {
+            name: "",
+            phone: "",
+            email: "",
+            comment: ""
+        };
+    };
+    $scope.usercontact = function (data) {
+
+        MyServices.usercontact("", data.name, data.email, data.phone, data.comment).success(contactfun);
+        $scope.showPopup();
+    };
+
+})
+
+.controller('CheckoutCtrl', function ($scope, MyServices) {
+    var ontotalsuccess = function (data, status) {
+        $scope.gettotal = data;
+    };
+    MyServices.totalcart().success(ontotalsuccess);
+    $scope.showshippingmethods = 0;
+    $scope.diffadd = false;
+    $scope.form = {};
+    $scope.form.shipdifferent = 1;
+    $scope.showdiffaddress = function () {
+        $scope.diffadd = true;
+    };
+
+    //check out chart
+    var onproductsuccess = function (data, status) {
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].id == data.product.id) {
+                $scope.products[i].image = data.productimage[0].image;
+            }
+        }
+    };
+
+    var onsuccess = function (data, status) {
+        $scope.products = data;
+
+        for (var i = 0; i < data.length; i++) {
+            MyServices.getproductdetails(data[i].id).success(onproductsuccess);
+        }
+
+    };
+    MyServices.getcart().success(onsuccess);
+    //check out chart
+
+
+//    var handler = StripeCheckout.configure({
+//        key: 'pk_live_I1udSOaNJK4si3FCMwvHsY4g',
+//        //key: 'pk_test_4etgLi16WbODEDr4YBFdcbP0',
+//        image: 'img/logo.jpg',
+//        currency: 'GBP',
+//        token: function (token) {
+//            MyServices.chargestripe(token.id, $scope.form.email, ($scope.subtotal + $scope.form.shippingcost - $scope.discountamount), ($scope.form.firstname + " " + $scope.form.lastname)).success(paymentcomplete);
+//            //window.location.href="http://www.lylaloves.co.uk/#/thankyou";
+//            // Use the token to create the charge with a server-side script.
+//            // You can access the token ID with `token.id`
+//        }
+//    });
+//
+//    $scope.StipePaymentGen = function (amount) {
+//
+//
+//        handler.open({
+//            name: 'Lyla Loves',
+//            description: 'Total Amount: £ ' + amount,
+//            amount: amount * 100,
+//
+//        });
+//
+//    };
+
+    $scope.newquantity = [];
+    var showcart = function (data, status) {
+        console.log(data);
+        $scope.cart = data;
+        console.log($scope.cart[0].qty);
+        console.log($scope.cart.length);
+
+        for (var i = 0; i < $scope.cart.length; i++) {
+            $scope.newquantity[i] = $scope.cart[i].qty;
+            console.log($scope.newquantity[i]);
+        }
+    };
+    MyServices.getcart().success(showcart);
+    var getsubtotal = function (data, status) {
+        console.log(data);
+        $scope.subtotal = parseFloat(data);
+        calcdiscountamount();
+
+    };
+    MyServices.totalcart().success(getsubtotal);
+    // free
+    $scope.free = function (country, subtotal, shipping) {
+        console.log("MAaaaza");
+        console.log(country);
+        console.log(subtotal);
+        console.log(shipping);
+        var coupondetails = MyServices.getcoupondetails();
+        console.log("coupon,,,,,");
+        console.log(coupondetails);
+        if (coupondetails && MyServices.getcoupondetails().coupontype == "4") {
+            console.log("ABC");
+            $scope.showshippingmethods = 5;
+            $scope.form.shippingcost = 0;
+            $scope.form.shippingmethod = 6;
+            return 0;
+        } else if (shipping == "1") {
+            if (country == "United Kingdom") {
+                if (subtotal >= 15) {
+                    $scope.showshippingmethods = 1;
+                    $scope.form.shippingmethod = 1;
+                    $scope.form.shippingcost = 0;
+                } else {
+                    $scope.showshippingmethods = 2;
+                    $scope.form.shippingmethod = 2;
+                    $scope.form.shippingcost = 3;
+                }
+
+            } else {
+                if (subtotal >= 20) {
+                    $scope.showshippingmethods = 3;
+                    $scope.form.shippingmethod = 4;
+                    $scope.form.shippingcost = 0;
+                } else {
+                    $scope.showshippingmethods = 4;
+                    $scope.form.shippingmethod = 5;
+                    $scope.form.shippingcost = 5;
+                }
+
+            }
+        }
+
+    };
+    $scope.free2 = function (country, subtotal, shipping) {
+        console.log(country);
+        console.log(subtotal);
+        console.log(shipping);
+        var coupondetails = MyServices.getcoupondetails();
+        if (coupondetails && MyServices.getcoupondetails().coupontype == "4") {
+            $scope.showshippingmethods = 5;
+            $scope.form.shippingcost = 0;
+            $scope.form.shippingmethod = 6;
+            return 0;
+        } else if (shipping == "2") {
+            if (country == "United Kingdom") {
+                if (subtotal >= 15) {
+                    $scope.showshippingmethods = 1;
+                    $scope.form.shippingmethod = 1;
+                    $scope.form.shippingcost = 0;
+                } else {
+                    $scope.showshippingmethods = 2;
+                    $scope.form.shippingmethod = 2;
+                    $scope.form.shippingcost = 3;
+                }
+
+            } else {
+                if (subtotal >= 20) {
+
+                    $scope.showshippingmethods = 3;
+                    $scope.form.shippingmethod = 4;
+                    $scope.form.shippingcost = 0;
+                } else {
+                    $scope.showshippingmethods = 4;
+                    $scope.form.shippingmethod = 5;
+                    $scope.form.shippingcost = 5;
+                }
+
+            }
+        }
+
+    };
+    // free
+    $scope.form.shippingcost = 0;
+    $scope.changeshippingcost = function (value) {
+        console.log(value);
+        $scope.form.shippingcost = value;
+    };
+
+    var paymentcomplete = function (data, status) {
+        console.log(data);
+        window.location.href = "http://www.lylaloves.co.uk/#/thankyou";
+    };
+
+    var handler = StripeCheckout.configure({
+        key: 'pk_live_I1udSOaNJK4si3FCMwvHsY4g',
+        //key: 'pk_test_4etgLi16WbODEDr4YBFdcbP0',
+        image: 'img/logo.jpg',
+        currency: 'GBP',
+        token: function (token) {
+            MyServices.chargestripe(token.id, $scope.form.email, ($scope.subtotal + $scope.form.shippingcost - $scope.discountamount), ($scope.form.firstname + " " + $scope.form.lastname)).success(paymentcomplete);
+            //window.location.href="http://www.lylaloves.co.uk/#/thankyou";
+            // Use the token to create the charge with a server-side script.
+            // You can access the token ID with `token.id`
+        }
+    });
+
+    $scope.StipePaymentGen = function (amount) {
+        console.log("amount:" + amount);
+        handler.open({
+            name: 'Lyla Loves',
+            description: 'Total Amount: £ ' + amount,
+            amount: amount * 100,
+
+        });
+    };
+
+    //place order
+    var orderemailsend = function (data, status) {
+        console.log(data);
+        //alert("Email send");
+    };
+    var orderplaced = function (data, status) {
+        console.log("place order returns");
+        console.log(data);
+        console.log($scope.form.email);
+        MyServices.orderemail($scope.form.email, data).success(orderemailsend);
+        alert("Order Placed");
+    };
+    $scope.continuepayment = function (form) {
+        $scope.paywithcard = 1;
+        $scope.form.finalamount = $scope.subtotal;
+        console.log($scope.cart);
+        //MainJson.orderitem($scope.cart);
+        $scope.form.cart = $scope.cart;
+        $scope.form.user = $scope.id;
+        $scope.form.status = $scope.status; //MainJson.placeorder(form.firstname,form.lastname,form.email,form.company,form.billingaddress,form.billingcity,form.billingstate,form.billingpincode,form.billingcountry,form.phone,form.fax,form.shippingaddress,form.shippingcity,form.shippingstate,form.shippingpincode,form.shippingcountry,$scope.id,$scope.status).success(orderplaced); 
+        MyServices.placeorder(form).success(orderplaced);
+    }
+
+})
+
+.controller('LoginCtrl', function ($scope, $location, MyServices) {
+    //Authenticate
+    $scope.useremail = 0;
+    //$scope.android="Android";
+
+    var cartdata = function (data, status) {};
+    var authenticate = function (data, status) {
+        console.log(data);
+        MyServices.getusercart(data.id).success(cartdata);
+        if (data != "false") {
+            $scope.loginlogouttext = "Logout";
+            $scope.getlogin2 = false;
+            $scope.useremail = data.email;
+        }
+    };
+    MyServices.authenticate().success(authenticate);
+
+    var emailsend = function (data, status) {
+        console.log(data);
+        alert("Email send to you");
+    };
+
+    //login
+    $scope.emptydata = "";
+    $scope.getlogin2 = true;
+    $scope.useremail = MyServices.getuseremail();
+
+    var getlogin = function (data, status) {
+        $scope.emptydata = data;
+        if (data != "false") {
+            //$scope.msg = "Login Successful";
+            $scope.useremail = data.email;
+            MyServices.setuseremail($scope.useremail);
+            $scope.getlogin2 = false;
+            $scope.login.email = " ";
+            $scope.login.password = " ";
+            //  $location.path('/#/tab/account/login');
+        } else {
+            $scope.msg = "Invalid Email Or Password";
+        }
+    };
+
+    $scope.userlogin = function (login) {
+        console.log(login);
+        MyServices.loginuser(login.email, login.password).success(getlogin);
+    };
+
+    //logout
+    var logoutsuccess = function (data, status) {
+        if (data != "false") {
+            $scope.getlogin2 = true;
+            $scope.emptydata = "false";
+            MyServices.setuseremail(" ");
+        };
+    }
+    $scope.logout = function () {
+        MyServices.logout().success(logoutsuccess);
+    };
+
+    //Signup
+    var getsignup = function (data, status) {
+        if (data != "false") {
+            $scope.msgr = "Registred Successful";
+            $location.url("#/account/login");
+            MyServices.signupemail(data.email).success(emailsend);
+        } else {
+            $scope.msgr = "Error In Registration";
+        }
+    };
+    $scope.signup = function (register) {
+        console.log(register);
+        MyServices.registeruser(register.firstname, register.lastname, register.email, register.password).success(getsignup);
+    };
+
+})
+
+.controller('OrderCtrl', function ($scope) {});
---
+++ js/services.js
@@ -0,0 +1,194 @@
+var adminurl = 'http://lyla@lylaloves.co.uk/admin/index.php/json/';
+
+var myservices = angular.module('myservices', [])
+
+.factory('MyServices', function ($http, $location) {
+    var retailer = 0;
+    var category = 0;
+    var useremail ="";
+    var coupondetails=$.jStorage.get("coupon");
+    var discount=$.jStorage.get("coupon");
+    return {
+        getuseremail: function() {
+            return useremail;
+        },
+        setuseremail: function(user) {
+            useremail = user;
+        },
+        getcoupondetails: function () {
+            return coupondetails;
+        },
+        setcoupondetails: function (coupon) {
+            $.jStorage.set("coupon",coupon);
+            coupondetails=coupon;
+        },
+        getdiscountcoupon: function (couponcode) {
+            return $http.post(adminurl + 'getdiscountcoupon?couponcode=' + couponcode, {}, {
+                withCredentials: true
+            });
+        },
+        getproductdetails: function (product, category) {
+            return $http.get(adminurl + 'getproductdetails', {
+                params: {
+                    product: product
+                }
+            }, {
+                withCredentials: true
+            });
+        },
+        getproductbycategory: function (category) {
+            return $http.get(adminurl + 'getproductbycategory', {
+                params: {
+                    category: category
+                }
+            }, {
+                withCredentials: true
+            });
+        },
+        addtocart: function (id, name, price, quantity) {
+            return $http.post(adminurl + 'addtocart?product=' + id + '&productname=' + name + "&quantity=" + quantity + "&price=" + price, {}, {
+                withCredentials: true
+            });
+        },
+        getcart: function () {
+            return $http.post(adminurl + 'showcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        gettotalcart: function () {
+            return $http.post(adminurl + 'totalitemcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        totalcart: function () {
+            return $http.post(adminurl + 'totalcart', {}, {
+                withCredentials: true
+            });
+            //return cart;
+        },
+        deletecart: function (id) {
+
+            subtotal = this.calcsubtotal();
+            return subtotal;
+        },
+        deletecartfromsession: function (id) {
+            return $http.post(adminurl + 'deletecart?id=' + id, {}, {
+                withCredentials: true
+            });
+        },
+        savecart: function (uid, id, quantity) {
+            console.log(cart);
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].id == id) {
+                    cart[i].quantity = quantity;
+                    console.log(cart[i].name);
+                    returntwo.state = $http.post(adminurl + 'addtocart?user=' + uid + '&product=' + id + "&quantity=" + cart[i].quantity, {}, {
+                        withCredentials: true
+                    });
+                }
+
+            }
+            console.log(cart);
+            returntwo.subtotal = this.calcsubtotal();
+            return returntwo;
+        },
+        calcsubtotal: function () {
+            subtotal = 0;
+            for (var i = 0; i < cart.length; i++) {
+                subtotal += cart[i].price * cart[i].quantity;
+            }
+            console.log(subtotal);
+            return subtotal;
+
+        },
+        gettotalproductsincart: function (data, status) {
+            console.log(data);
+            TemplateService.totalproducts = data;
+            return 0;
+        },
+        usercontact: function (id, name, email, phone, comment) {
+            return $http.post(adminurl + 'usercontact?id=' + id + '&name=' + name + '&email=' + email + '&phone=' + phone + '&comment=' + comment, {}, {
+                withCredentials: true
+            });
+        },
+
+        placeorder: function (form) {
+            return $http({
+                url: adminurl + 'placeorder',
+                method: "POST",
+                withCredentials: true,
+                data: {
+                    'form': form
+                }
+            });
+        },
+        seach: function (search) {
+            return $http.post(adminurl + 'searchbyname?search=' + search, {}, {
+                withCredentials: true
+            });
+        },
+        showwishlist: function (user) {
+            return $http.post(adminurl + 'showwishlist?user=' + user, {}, {
+                withCredentials: true
+            });
+        },
+        logout: function () {
+            return $http.post(adminurl + 'logout', {}, {
+                withCredentials: true
+            });
+        },
+        addtowishlist: function (user, product) {
+            return $http.post(adminurl + 'addtowishlist?user=' + user + '&product=' + product, {}, {
+                withCredentials: true
+            });
+        },
+        authenticate: function () {
+            return $http.post(adminurl + 'authenticate', {}, {
+                withCredentials: true
+            });
+        },
+        registeruser: function (firstname, lastname, email, password) {
+            return $http.post(adminurl + 'registeruser?firstname=' + firstname + '&lastname=' + lastname + '&email=' + email + '&password=' + password, {}, {
+                withCredentials: true
+            });
+        },
+        loginuser: function (email, password) {
+            return $http.post(adminurl + 'loginuser?email=' + email + '&password=' + password, {}, {
+                withCredentials: true
+            });
+        },
+        signupemail: function (email) {
+            return $http.post(adminurl + 'signupemail?email=' + email, {}, {
+                withCredentials: true
+            });
+        },
+        orderemail: function (email, orderid) {
+            return $http.post(adminurl + 'orderemail?email=' + email + '&orderid=' + orderid, {}, {
+                withCredentials: true
+            });
+        },
+        getusercart: function (user) {
+            return $http.get(adminurl + 'getusercart?user=' + user, {}, {
+                withCredentials: true
+            });
+        },
+        getallslider: function (user) {
+            return $http.get(adminurl + 'getallslider');
+        },
+        chargestripe: function (token, email,amount,name) {
+            return $http.get('http://wohlig.com/stripe/index.php/welcome/chargestripe', {
+                params: {
+                    token: token,
+                    email: email,
+                    amount: amount*100,
+                    name: name,
+
+                }
+            }, {
+                withCredentials: true
+            });
+        },
+    }
+});
